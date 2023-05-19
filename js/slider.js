9baa5cbe607c6d51fcf136a611eb94ca154b8586
K4U4B0MFIM <127872501+KauaBomfim@users.noreply.github.com>
2023-05-18 22:32:11 -0300

    adicionado mais conteúdo na home

--- js/slider.js
+++ js/slider.js
@@ -17,3 +17,17 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
