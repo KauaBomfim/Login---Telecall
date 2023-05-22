3fa100a279eca722e70223397f0a54629f5f9c9f
K4U4B0MFIM <127872501+KauaBomfim@users.noreply.github.com>
2023-05-22 20:44:15 -0300

    home finalizada 100%

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+var radio = document.querySelector('.manual_btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 6000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 4){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
